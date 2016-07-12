0cd04bbfe4ff6675bebadef90579e1f6cbd801a7
Elena <elena.corni@cnaf.infn.it>
2016-07-12 18:35:18 +0200

    + flag readFromFile, + aggiorn. help, + read from last file

--- dump_and_read.cpp
+++ dump_and_read.cpp
@@ -42,6 +42,7 @@ int main ( int argc, char *argv[] ){
         int ierr;
         int p;
         double wtime;
+	bool readFromFile;
 
 	//  Initialize MPI.
         ierr = MPI_Init ( &argc, &argv );
@@ -62,6 +63,7 @@ int main ( int argc, char *argv[] ){
             		("size",	po::value<float>(&mb)->required(),		"size to be copied (MB)")
 	    		("seconds",	po::value<float>(&sec)->default_value(60),	"seconds to wait between two iterations")
 	    		("iterations",	po::value<int>(&its)->default_value(1),		"iterations of process")
+			("readFromFile",po::value<bool>(&readFromFile),			"enable to read from files (on/off, true/false, 1/0)")
         	;
 
         	po::positional_options_description p;
@@ -69,15 +71,16 @@ int main ( int argc, char *argv[] ){
 		p.add("size", 1);
 		p.add("seconds", 1);
 		p.add("iterations", 1);
+		p.add("readFromFile",1);
 
         	po::variables_map vm;
         	po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
 		
         	if (vm.count("help")) {
 			if(id == 0){
-            			cout << "\nUsage: mpirun -np <n_process> ./a.out [--size ARG]* [--seconds ARG] [--iterations ARG]\n";
+            			cout << "\nUsage: mpirun -np <n_process> ./a.out [--size ARG]* [--seconds ARG] [--iterations ARG] [--readFromFile ARG]\n";
 				cout << "   ...or...\n";
-				cout << "       mpirun -np <n_process> ./a.out n_mb* n_seconds n_iterations\n";
+				cout << "       mpirun -np <n_process> ./a.out n_mb* n_seconds n_iterations readFromFile\n";
 	    			cout << "* Required Argument\n\n";
             			cout << desc << "\n";
 			}
@@ -102,6 +105,7 @@ int main ( int argc, char *argv[] ){
 			std::cout << "ERROR SIZE!! Please choose size < 1000000 MB"<< std::endl;
 		}
 		else{
+			cout << "readFromFile = "<<readFromFile<<"\n";
 			byte = mb * 1000000;
 			size_byte = (int)std::ceil(byte);
 		
@@ -109,39 +113,42 @@ int main ( int argc, char *argv[] ){
 			
 			//for each process, create a vector (size = tot iterations) that it will contain delta write times of each iteration.
 			std::vector<double> delta_wp(its);
+		//	std::vector<double> vector_start(size_byte);			
 
 			for(i = 0; i < its; i++){
 				if(id == 0){
 					cout << "\n";				
 				}
 			
-			//	ifstream infile;
-			//	infile.open("/dev/zero" ,ios::binary|ios::in);
-			//	outfile.open(name_output_file, ios::binary|ios::out);
-
 				/* when execution leaves this scope,
 				   it is guaranteed that vector object will be destructed and memory will be freed.*/
 				{
 					std::vector<char> buffer(size_byte);
-					double start_write = 0.;
-					//infile.read(buffer,byte);
+					
+					if(readFromFile && i>0){
+						std::string inputFile = "outFile_it" + std::to_string(i-1) + "_p" + std::to_string(id)+".txt";
+						ifstream infile(inputFile, std::ios::binary);
+						buffer.assign((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+						cout << "I read " << mb << " MB from file " << inputFile << "\n";
+					}
 
 					//synchronization: no process can pass the barrier until all of them call the function.
 					MPI_Barrier(MPI_COMM_WORLD);
-					
+
+					double start_write = 0.;
+
 					std::ofstream outfile;
-					std::string name_output_file = "outFile_it" + std::to_string(i) + "_p" + std::to_string(id)+".txt";
+					std::string outputFile = "outFile_it" + std::to_string(i) + "_p" + std::to_string(id)+".txt";
 					
 					start_write = MPI_Wtime ( );
-					outfile.open(name_output_file, ios::binary|ios::out);
+					outfile.open(outputFile, ios::binary|ios::out);
 					
 					copy(buffer.begin(), buffer.end(), std::ostreambuf_iterator<char>(outfile));	
 
-					//infile.close();
 					outfile.close();
 					
 					delta_wp[i] = MPI_Wtime ( ) - start_write;
-					cout << "I wrote " << mb << " MB to file " << name_output_file << " in "<< delta_wp[i] <<" seconds.\n";
+					cout << "I wrote " << mb << " MB to file " << outputFile << " in "<< delta_wp[i] <<" seconds.\n";
 				}
 				wait (sec);
 			}
